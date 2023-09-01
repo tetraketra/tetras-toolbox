35649c7d03f105d8d1f19bf5a6e733783ce37455
Tetra Silverberg <tetra.silv@gmail.com>
2023-09-01 00:17:48 -0400

    updated fixed_point to have multiple behaviors

--- tetras_toolbox/fixed_point.py
+++ tetras_toolbox/fixed_point.py
@@ -2,32 +2,50 @@ import itertools      as  it
 import more_itertools as mit
 import operator       as op
 from   ._type_t       import T
-from   typing         import Callable, Iterator
+from   typing         import Callable, Iterator, Literal
 
 
-def fixed_point(base_state: T, evolution_func: Callable[[T], T], compare_func: Callable[[T, T], bool] = op.eq) -> Iterator[T]:
-    """
-    Generates the fixed point of a function starting from a base state. 
+def fixed_point(
+   base_state: T, 
+   evolution_func: Callable[[T], T], 
+   compare_func: Callable[[T, T], bool] = op.eq, 
+   return_mode: Literal['value', 'infiter', 'callable'] = 'callable'
+) -> T | Iterator[T] | Callable[[], T]:
+   
+   """
+   Generates the fixed point of a function starting from a base state. 
 
-    Args:
-    - `evolution_func: Callable`: The function which is repeatedly applied.
-    - `base_state: T`: The starting state for the evolution function. 
-       Must implement `compare_func`. 
-       Defaults to `__eq__`. 
+   Args:
+   - `evolution_func: Callable`: The function which is repeatedly applied.
+   - `base_state: T`: The starting state for the evolution function. 
+     Must implement `compare_func`. 
+   - `compare_func: Callable[[T, T], bool]`: The comparison function. 
+     Defaults to `__eq__`.
+   - `return_mode: Literal['value', 'infiter', 'callable']`: The requested
+     behavior of the returned value.
 
-    Returns:
-    - `_: Iterator[T]`: The fixed point of the evolved function.
+   Returns:
+   - `_: T|Iterator[T]|Callable[[],T]`: The fixed point of the evolved function
+     as either the value itself, an infinite iterator (for use in iterchains),
+     or a callable that returns the value.
 
-    Notes:
-    - May iterate indefinitely. This is considered user error.
-    """
+   Notes:
+   - May iterate indefinitely. This is considered user error.
+   """
 
-    return filter(lambda ab: ab[0] == ab[1], mit.windowed(it.islice(it.accumulate(it.repeat(base_state), lambda x, _: evolution_func(x)), 1, None), 2))
+   r = filter(lambda ab: compare_func(ab[0], ab[1]), mit.windowed(it.islice(it.accumulate(it.repeat(base_state), lambda x, _: evolution_func(x)), 1, None), 2))
 
-    # alternatively, if you're not as cool...  
-    # r = it.accumulate(it.repeat(base_state), lambda x, _: evolution_func(x)) # functional way of calling the `evolution_func` successively on `base_state`
-    # r = it.islice(r, 1, None)                                                # chop off the first result because it's the `base_state`, not an iterator
-    # r = mit.windowed(r, 2)                                                   # moving window, such that you can use `compare_func` current and previous
-    # r = filter(lambda ab: compare_func(ab[0], ab[1]), r)                     # only return windows which pass the `compare_func`
-    # r = next(r)[0]                                                           # return the left side of the first such window, i.e. the fixed point
-    # return r
+   match return_mode:
+      case 'value':
+         return next(r)[0]
+      case 'callable':
+         return lambda:next(r)[0]
+      case 'infiter':
+         return map(op.getitem, r, it.repeat(1))
+
+   # alternatively, if you're not as cool...  
+   #  r = it.accumulate(it.repeat(base_state), lambda x, _: evolution_func(x)) # functional way of calling the `evolution_func` successively on `base_state`
+   #  r = it.islice(r, 1, None)                                                # chop off the first result because it's the `base_state`, not an iterator
+   #  r = mit.windowed(r, 2)                                                   # moving window, such that you can use `compare_func` current and previous
+   #  r = filter(lambda ab: compare_func(ab[0], ab[1]), r)                     # only return windows which pass the `compare_func`.
+   # at this point, r is an infinite filter pairs of fixed points
